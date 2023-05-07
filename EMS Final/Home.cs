6c4b0a61f175c0ba74e7532e928347b5dab5bc44
HamzaMiraz <amirhamza11170@gmail.com>
2023-05-08 00:58:33 +0600

    first commit

---
+++ EMS Final/Home.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EMS_Final
+{
+    public partial class Home : Form
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.Show();
+            this.Hide();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            Employee emp = new Employee();
+            emp.Show();
+            this.Hide();
+        }
+
+        private void label8_Click(object sender, EventArgs e)
+        {
+            Login log = new Login();
+            log.Show();
+            this.Hide();
+
+        }
+        private void pictureBox7_Click(object sender, EventArgs e)
+        {
+            Login log = new Login();
+            log.Show();
+            this.Hide();
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            ViewEmployee vemp = new ViewEmployee();
+            vemp.Show();
+            this.Hide();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            ViewEmployee vemp = new ViewEmployee();
+            vemp.Show();
+            this.Hide();
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            Salary salary = new Salary();
+            salary.Show();
+            this.Hide();                                                                                    
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            Salary salary = new Salary();
+            salary.Show();
+            this.Hide();
+        }
+    }
+}
